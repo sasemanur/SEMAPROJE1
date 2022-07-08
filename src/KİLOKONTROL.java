9d46415bfc7379c7e6df101bfe400f347eadb7b0
sasemanur <sa.semanurarslan@gmail.com>
2022-07-08 14:25:48 +0300

    1. değişiklik yapıldı 2.değişiklik yapıldı

--- src/KİLOKONTROL.java
+++ src/KİLOKONTROL.java
@@ -10,15 +10,17 @@ public class KİLOKONTROL {
 
         System.out.print("boyunuzu giriniz = ");
         int boy1= input.nextInt();
+        System.out.print("yaşınızı giriniz = ");
+        int yaş= input.nextInt();
 
         double kıtleIndeksi1= kg/(boy1*boy1);
         System.out.println("kıtleIndeksi1 = " + kıtleIndeksi1);
 
 
         if (kıtleIndeksi1 >  0.0025510926118626432){
-            System.out.println("ŞİRİN BİR PATATES OLMAMAK İÇİN KİLO VERMENİZ LAZIM ");
+            System.out.println("ŞİRİN BİR PATATES OLMAMAK İÇİN KİLO VERMENİZ LAZIM " + "yaş = "  +yaş);
         }else{
-            System.out.println("KİLONUZ İDEAL  :) NOT: SAĞLIĞINIZ İÇİN KİLONUZU KORUYUNUZ ");
+            System.out.println("KİLONUZ İDEAL  :) NOT: SAĞLIĞINIZ İÇİN KİLONUZU KORUYUNUZ " + "yaş = " + yaş);
         }
         // bu bir kilo kontrol projesidir.
     }
